e7c20c919cc91f4db20b379b5ddea3464fd99cae
Rob Richard <rob@1stdibs.com>
2020-01-13 10:47:07 -0500

    Remove support for `applyToPrevious`
    All patches will be returned as provided by the server.

--- src/PatchResolver.js
+++ src/PatchResolver.js
@@ -1,45 +1,7 @@
 import { parseMultipartHttp } from './parseMultipartHttp';
 
-function insertPatch(obj, path, data) {
-    if (Array.isArray(obj) && typeof path === 'number') {
-        return [].concat(obj.slice(0, path), [data], obj.slice(path + 1));
-    } else {
-        return {
-            ...obj,
-            [path]: data,
-        };
-    }
-}
-
-// recursive function to apply the patch to the previous response
-function applyPatch(previousResponse, patchPath, patchData) {
-    const [nextPath, ...rest] = patchPath;
-    if (rest.length === 0) {
-        return insertPatch(previousResponse, nextPath, patchData);
-    }
-    return insertPatch(
-        previousResponse,
-        nextPath,
-        applyPatch(previousResponse[nextPath], rest, patchData)
-    );
-}
-
-function mergeErrors(previousErrors, patchErrors) {
-    if (previousErrors && patchErrors) {
-        return [].concat(previousErrors, patchErrors);
-    } else if (previousErrors) {
-        return previousErrors;
-    } else if (patchErrors) {
-        return patchErrors;
-    }
-    return undefined;
-}
-
-export function PatchResolver({ onResponse, applyToPrevious, mergeExtensions = () => {} }) {
-    this.applyToPrevious = typeof applyToPrevious === 'boolean' ? applyToPrevious : true;
+export function PatchResolver({ onResponse }) {
     this.onResponse = onResponse;
-    this.mergeExtensions = mergeExtensions;
-    this.previousResponse = null;
     this.processedChunks = 0;
     this.chunkBuffer = '';
 }
@@ -49,27 +11,6 @@ PatchResolver.prototype.handleChunk = function(data) {
     const { newBuffer, parts } = parseMultipartHttp(this.chunkBuffer);
     this.chunkBuffer = newBuffer;
     if (parts.length) {
-        if (this.applyToPrevious) {
-            parts.forEach(part => {
-                if (this.processedChunks === 0) {
-                    this.previousResponse = part;
-                } else {
-                    if (!(Array.isArray(part.path) && typeof part.data !== 'undefined')) {
-                        throw new Error('invalid patch format ' + JSON.stringify(part, null, 2));
-                    }
-                    this.previousResponse = {
-                        ...this.previousResponse,
-                        data: applyPatch(this.previousResponse.data, part.path, part.data),
-                        errors: mergeErrors(this.previousResponse.errors, part.errors),
-                        extensions: this.mergeExtensions(this.previousResponse.extensions, part.extensions),
-                    };
-                }
-                this.processedChunks += 1;
-            });
-            // don't need to re-trigger every intermediate state
-            this.onResponse(this.previousResponse);
-        } else {
-            parts.forEach(part => this.onResponse(part));
-        }
+        parts.forEach(part => this.onResponse(part));
     }
 };
