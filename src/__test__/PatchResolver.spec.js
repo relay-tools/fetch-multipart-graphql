f5b20599f601ffee6f78ded2e59a86e64ba96c20
Rob Richard <rob@1stdibs.com>
2020-01-14 10:23:50 -0500

    update tests and remove snapshots

--- src/__test__/PatchResolver.spec.js
+++ src/__test__/PatchResolver.spec.js
@@ -4,61 +4,49 @@ import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-const chunk1 = [
-    '',
-    '---',
-    'Content-Type: application/json',
-    'Content-Length: 142',
-    '',
-    '{"data":{"viewer":{"currencies":null,"user":{"profile":null,"items":{"edges":[{"node":{"isFavorite":null}},{"node":{"isFavorite":null}}]}}}}}\n',
-].join('\r\n');
+function getMultiPartResponse(data) {
+    const json = JSON.stringify(data);
+    const chunk = Buffer.from(json, 'utf8');
 
-const chunk1error = [
-    '',
-    '---',
-    'Content-Type: application/json',
-    'Content-Length: 104',
-    '',
-    '{"data":{"viewer":{"currencies":null,"user":{"profile":null}}},"errors":[{"message":"Very Bad Error"}]}\n',
-].join('\r\n');
+    return [
+        '',
+        '---',
+        'Content-Type: application/json',
+        `Content-Length: ${String(chunk.length)}`,
+        '',
+        json,
+        '',
+    ].join('\r\n');
+}
 
-const chunk2 = [
-    '',
-    '---',
-    'Content-Type: application/json',
-    'Content-Length: 85',
-    '',
-    '{"path":["viewer","currencies"],"data":["USD","GBP","EUR","CAD","AUD","CHF","😂"]}\n', // test unicode
-].join('\r\n');
+const chunk1Data = {
+    data: {
+        viewer: {
+            currencies: null,
+            user: {
+                profile: null,
+                items: { edges: [{ node: { isFavorite: null } }, { node: { isFavorite: null } }] },
+            },
+        },
+    },
+};
+const chunk1 = getMultiPartResponse(chunk1Data);
 
-const chunk2error = [
-    '',
-    '---',
-    'Content-Type: application/json',
-    'Content-Length: 127',
-    '',
-    '{"path":["viewer","currencies"],"data":["USD","GBP","EUR","CAD","AUD","CHF","😂"],"errors":[{"message":"Not So Bad Error"}]}\n',
-].join('\r\n');
+const chunk2Data = {
+    path: ['viewer', 'currencies'],
+    data: ['USD', 'GBP', 'EUR', 'CAD', 'AUD', 'CHF', '😂'], // test unicode
+    errors: [{ message: 'Not So Bad Error' }],
+};
+const chunk2 = getMultiPartResponse(chunk2Data);
 
-const chunk3 = [
-    '',
-    '---',
-    'Content-Type: application/json',
-    'Content-Length: 76',
-    '',
-    '{"path":["viewer","user","profile"],"data":{"displayName":"Steven Seagal"}}\n',
-].join('\r\n');
+const chunk3Data = { path: ['viewer', 'user', 'profile'], data: { displayName: 'Steven Seagal' } };
+const chunk3 = getMultiPartResponse(chunk3Data);
 
-const chunk4 = [
-    '',
-    '---',
-    'Content-Type: application/json',
-    'Content-Length: 78',
-    '',
-    '{"data":false,"path":["viewer","user","items","edges",1,"node","isFavorite"]}\n',
-    '',
-    '-----\r\n',
-].join('\r\n');
+const chunk4Data = {
+    data: false,
+    path: ['viewer', 'user', 'items', 'edges', 1, 'node', 'isFavorite'],
+};
+const chunk4 = getMultiPartResponse(chunk4Data);
 
 describe('PathResolver', function() {
     it('should work on each chunk', function() {
@@ -68,19 +56,19 @@ describe('PathResolver', function() {
         });
 
         resolver.handleChunk(chunk1);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk1Data]);
 
         onResponse.mockClear();
         resolver.handleChunk(chunk2);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk2Data]);
 
         onResponse.mockClear();
         resolver.handleChunk(chunk3);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk3Data]);
 
         onResponse.mockClear();
         resolver.handleChunk(chunk4);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk4Data]);
     });
 
     it('should work when chunks are split', function() {
@@ -98,7 +86,7 @@ describe('PathResolver', function() {
         resolver.handleChunk(chunk1b);
         expect(onResponse).not.toHaveBeenCalled();
         resolver.handleChunk(chunk1c);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk1Data]);
         onResponse.mockClear();
 
         const chunk2a = chunk2.substr(0, 35);
@@ -107,7 +95,7 @@ describe('PathResolver', function() {
         resolver.handleChunk(chunk2a);
         expect(onResponse).not.toHaveBeenCalled();
         resolver.handleChunk(chunk2b);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk2Data]);
         onResponse.mockClear();
 
         const chunk3a = chunk3.substr(0, 10);
@@ -119,7 +107,7 @@ describe('PathResolver', function() {
         resolver.handleChunk(chunk3b);
         expect(onResponse).not.toHaveBeenCalled();
         resolver.handleChunk(chunk3c);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk3Data]);
     });
 
     it('should work when chunks are combined', function() {
@@ -129,7 +117,7 @@ describe('PathResolver', function() {
         });
 
         resolver.handleChunk(chunk1 + chunk2);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk1Data, chunk2Data]);
     });
 
     it('should work when chunks are combined and split', function() {
@@ -143,13 +131,13 @@ describe('PathResolver', function() {
         const chunk3c = chunk3.substr(11 + 20);
 
         resolver.handleChunk(chunk1 + chunk2 + chunk3a);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk1Data, chunk2Data]);
         onResponse.mockClear();
 
         resolver.handleChunk(chunk3b);
         expect(onResponse).not.toHaveBeenCalled();
         resolver.handleChunk(chunk3c);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk3Data]);
     });
 
     it('should work when chunks are combined across boundaries', function() {
@@ -162,42 +150,9 @@ describe('PathResolver', function() {
         const chunk2b = chunk2.substring(35);
 
         resolver.handleChunk(chunk1 + chunk2a);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk1Data]);
         onResponse.mockClear();
         resolver.handleChunk(chunk2b);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
-    });
-
-    it('should merge errors', function() {
-        const onResponse = jest.fn();
-        const resolver = new PatchResolver({
-            onResponse,
-        });
-
-        resolver.handleChunk(chunk1error);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
-        onResponse.mockClear();
-        resolver.handleChunk(chunk2error);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
-        onResponse.mockClear();
-        resolver.handleChunk(chunk3);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
-    });
-
-    it('should work when not applying to previous', function() {
-        const onResponse = jest.fn();
-        const resolver = new PatchResolver({
-            onResponse,
-            applyToPrevious: false,
-        });
-
-        const chunk2a = chunk2.substring(0, 35);
-        const chunk2b = chunk2.substring(35);
-
-        resolver.handleChunk(chunk1 + chunk2a);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
-        onResponse.mockClear();
-        resolver.handleChunk(chunk2b);
-        expect(onResponse.mock.calls[0][0]).toMatchSnapshot();
+        expect(onResponse.mock.calls[0][0]).toEqual([chunk2Data]);
     });
 });
