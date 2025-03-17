d576a46562cbec45fa7957e58d1d254fa460db84
Rob Richard <rob@1stdibs.com>
2025-03-17 13:52:45 -0400

    return statusCode on json parse error (#51)

--- src/fetch.js
+++ src/fetch.js
@@ -42,6 +42,11 @@ export function fetchImpl(
                 return response.json().then((json) => {
                     onNext([json], { responseHeaders: response.headers });
                     onComplete();
+                }, (err) => {
+                    const parseError = err;
+                    parseError.response = response;
+                    parseError.statusCode = response.status;
+                    onError(parseError);
                 });
             }
         })
